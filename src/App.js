0f46d34b2927e3dee1fbb0a85efd5ce5e3d193bb
LeandroCantero <lcantero18@gmail.com>
2023-06-01 21:44:48 -0300

    save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Formulario from './Components/Formulario';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Fragment, useState } from 'react';
 
 function App() {
+
+  //Hook de estado vacio
+  const [clientes, setClientes] = useState([]);
+
+  const agregarCliente = (socio) => {
+    setClientes([
+      ...clientes,
+      socio
+    ])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Container>
+        <Row>
+          <Col><h3>Usuarios de la Veterinaria</h3></Col>
+        </Row>
+        <Row>
+          <Col>
+            <Formulario
+              agregarCliente={agregarCliente}
+            /></Col>
+          <Col><h3>Listado de usuarios</h3></Col>
+        </Row>
+      </Container>
+    </Fragment>
   );
 }
 
---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from 'react'
+import { Fragment } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import {v4 as uuid} from 'uuid';
+
+const Formulario = ({agregarCliente}) => {
+
+    const [socio, setSocio] = useState({
+        nombre: "",
+        dni: ""
+    })
+
+    const { nombre, dni } = socio;
+
+    const [error, setError] = useState(false);
+
+
+
+    const handleChange = (e) => {
+        setSocio({
+            ...socio,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        if(nombre.trim === '' || dni.trim() === ''){
+            setError(true);
+            return;
+        }
+
+        setError(false);
+
+        socio.id = uuid();
+
+        agregarCliente(socio)
+
+        setSocio({
+            nombre:"",
+            dni:""
+        })
+
+
+    }
+
+    return (
+        <Fragment>
+            <Form onSubmit={submitForm}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Nombre</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name='nombre'
+                        placeholder="Nombre completo"
+                        onChange={handleChange}
+                        value={nombre}
+                    />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>DNI</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Sin puntos ni espacios."
+                        name='dni'
+                        onChange={handleChange}
+                        value={dni}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Ingresar Socio
+                </Button>
+            </Form>
+
+            {
+                error 
+                && <h4>Completá todos los campos</h4>
+            }
+
+        </Fragment>
+    )
+}
+
+export default Formulario
